8f98c01cba5fa493a2db2902ec784ef3f38ad0b8
Tegar Adisaputra <tegara25@gmail.com>
2022-11-16 12:18:27 +0700

    pertama

---
+++ resources/views/user/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+@section('body')
+<!-- @include('layout.header') -->
+<div class="container mt-5">
+    <row>
+        <div class="col-md-6">
+            <h1>From the edge</h1>
+            <h1 class="mb-5">Reach around the globe.</h1>
+            <a href="#" class="btn1" style="text-decoration:none;">Explore</a>
+            <a href="#" class="btn2" style="text-decoration:none;">Get Started</a>
+        </div>
+        <div class="col-md-6"></div>
+    </row>
+</div>
+@endsection()
