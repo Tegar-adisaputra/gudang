3629c101d1dc4574c0cb907a148eb3660fc1a3cb
Tegar Adisaputra <tegara25@gmail.com>
2022-11-21 09:35:09 +0700

    add jetstream

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layout.main')
 @section('body')
 <!-- @include('layout.header') -->
+@if (Route::has('login'))
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        @auth
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+        @else
+            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+        @endauth
+    </div>
+@endif
 <div class="container mt-5">
     <row>
         <div class="col-md-6 home-started">
